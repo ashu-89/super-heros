335dce6eddd4c04628dbb463823b899e65e48bf8
ashutosh <ashu.p89@gmail.com>
2020-11-06 18:37:26 +0530

    onChange and API calls added

--- src/SearchBoxComp.js
+++ src/SearchBoxComp.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchBoxComp(props) {
+
+    const [searchText, setSearchText] = useState("");
+
+    async function callApi(){
+        const response = await fetch(`https://www.superheroapi.com/api.php/10224483035691240/search/${searchText}`);
+        const data = await response.json();
+        console.log("callApi -> data",data);
+
+    }
+    
+    function searchSuperHeroes(e){
+        const searchTerm = e.target.value;
+        console.log("searchSuperHeroes -> searchTerm", searchTerm);
+        setSearchText(searchTerm);
+        if(searchTerm.length > 3){
+            callApi();
+        }
+
+
+    }
+
+
     return (
         <div>
-            <input id="search-bar" type="search" placeholder="Hunt Super Heroes here ..."></input>
+            <input id="search-bar" 
+                type="search" 
+                placeholder="Hunt Super Heroes here ..."
+                onChange = {searchSuperHeroes}
+                value = {searchText}                
+            >
+            </input>
         </div>
     );
 }
